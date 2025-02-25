4755a7a28dde42ad0551a64464d8829912071533
Mrinal <cmrinal16@gmail.com>
2025-02-25 15:15:48 -0500

    make types visible

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { getSupportedSources } from './search/scraping';
 export { startAudit } from './main/index';
-export type { SearchResult, Job } from './types/audit.type';
+export type { SearchResult, Job, RawJob, ProcessedJob, ScrapedJob, SearchTool } from './types/audit.type';
 export type { SearchConfig, RoleType, SearchSource } from './types/config.type';
 export { configure } from './lib/utils';
